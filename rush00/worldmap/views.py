66bff65eb151b6a20cd894635e4fc99033adf8de
Oscar VERETOUT <overetou@e2r2p14.42.fr>
2020-01-26 09:59:18 +0100

    directionnal buttons now can be intercepted on the server

--- rush00/worldmap/views.py
+++ rush00/worldmap/views.py
@@ -1,18 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, Http404
 from django.conf import settings
+
 
 # Create your views here.
 
 def wordmap_f(request):
-    return render(
-                request, 
-                "worldmap.html", {
-                    'mapx': range(settings.GRID_SIZE),
-                    'mapy': range(settings.GRID_SIZE),
-                    'persox': 0,
-                    'persoy': 1,
-                    'balls_number': 7,
-                    'ballfindstring': "You encountered Shining",
-                    'moviefindstring': 'You just found a ball!'
-                    }
-    )
+    if request.method == "POST":
+        if 'up' in request.POST:
+            return HttpResponse("Post request received. You want to go up")
+        if 'down' in request.POST:
+            return HttpResponse("Post request received. You want to go down")
+        if 'left' in request.POST:
+            return HttpResponse("Post request received. You want to go left")
+        if 'right' in request.POST:
+            return HttpResponse("Post request received. You want to go right")
+        else:
+            raise Http404()
+    else:
+        return render(
+                    request, 
+                    "worldmap.html", {
+                        'mapx': range(settings.GRID_SIZE),
+                        'mapy': range(settings.GRID_SIZE),
+                        'persox': 0,
+                        'persoy': 1,
+                        'balls_number': 7,
+                        'ballfindstring': "You encountered Shining! Press ? to capture it!",
+                        'moviefindstring': 'You just found a ball!'
+                        }
+        )
