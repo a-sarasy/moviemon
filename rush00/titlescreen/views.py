952224d043905b4c70af83ab20cfe2f150395b2d
Oscar VERETOUT <overetou@e2r2p14.42.fr>
2020-01-27 15:44:22 +0100

    final?

--- rush00/titlescreen/views.py
+++ rush00/titlescreen/views.py
@@ -8,5 +8,4 @@ from common import movies,game
 def titlescreen_f(request):
     movies_obj = movies.Movies_info()
     movies_info_list = movies_obj.get_list()
-    #print(movies_info_list)
     return render(request, "titlescreen/titlescreen.html", {"worldmap_link": "worldmap?start=True", "load_link":"options/load_game"})
